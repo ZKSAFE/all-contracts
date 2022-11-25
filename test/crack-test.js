4c27a7e124b2ee9efe53797aa519dce8de825844
George <85392015@qq.com>
2022-11-25 11:15:05 +0800

    Create crack-test.js

---
+++ test/crack-test.js
@@ -0,0 +1,108 @@
+const { BigNumber, utils } = require('ethers')
+const snarkjs = require("snarkjs")
+const fs = require("fs")
+
+describe('deploy-test', function () {
+
+    //Filcoin Poseidon: RTX3070:47.17Mhash/s, Ryzen5900X:7.41Mhash/s
+    //ETH SHA3: RTX3090:120Mhash/s, Mac M1:1Mhash/s
+    //Ours: Mac M1 1thread:0.1Mhash/s
+    //Our estimate: Poseidon: RTX3090:100Mhash/s
+
+    const HashRate = 100000000
+
+    before(async function () {
+    })
+
+    it('6 chars', async function () {
+        //6 number
+        let sec = b(10).pow(6).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //6 number+english
+        sec = b(10+26+26).pow(6).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //6 number+english+symbol
+        sec = b(10+26+26+30).pow(6).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+    })
+
+    it('8 chars', async function () {
+        //8 number
+        let sec = b(10).pow(8).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //8 number+english
+        sec = b(10+26+26).pow(8).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //8 number+english+symbol
+        sec = b(10+26+26+30).pow(8).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+    })
+
+    it('10 chars', async function () {
+        //10 number
+        let sec = b(10).pow(10).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //10 number+english
+        sec = b(10+26+26).pow(10).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //10 number+english+symbol
+        sec = b(10+26+26+30).pow(10).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+    })
+
+    it('12 chars', async function () {
+        //12 number
+        let sec = b(10).pow(12).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //12 number+english
+        sec = b(10+26+26).pow(12).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        //12 number+english+symbol
+        sec = b(10+26+26+30).pow(12).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+    })
+
+    it('password vs privatekey', async function () {
+        let sec = b(92).pow(40).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+
+        sec = b(2).pow(256).div(HashRate)
+        console.log(s(sec) + 'sec', s(sec.div(86400)) + 'day', s(sec.div(86400).div(365)) + 'year')
+    })
+
+
+
+    async function delay(sec) {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, sec * 1000);
+        })
+    }
+
+    function m(num, decimals) {
+        return BigNumber.from(num).mul(BigNumber.from(10).pow(decimals))
+    }
+
+    function d(bn, decimals) {
+        return bn.mul(BigNumber.from(100)).div(BigNumber.from(10).pow(decimals)).toNumber() / 100
+    }
+
+    function b(num) {
+        return BigNumber.from(num)
+    }
+
+    function n(bn) {
+        return bn.toNumber()
+    }
+
+    function s(bn) {
+        return bn.toString()
+    }
+})
