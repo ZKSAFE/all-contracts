f2d74bd6992a7be00273ab608f04932ef055aa5e
George <85392015@qq.com>
2023-03-29 11:55:03 +0800

    Update callV2.js

--- scripts/callV2.js
+++ scripts/callV2.js
@@ -8,58 +8,84 @@ const snarkjs = require('snarkjs')
 const ZKPASS_ADDRESS = '0x6009234967B1c7872de00BB3f3e77610b8D6dc9e'
 const SAFEBOXFACTORY_ADDRESS = '0xa877a2247b318b40935E102926Ba5ff4F3b0E8b1'
 const USDC_ADDRESS = '0x67aE69Fd63b4fc8809ADc224A9b82Be976039509'
-const SAFEBOX_ADDRESS = '0xd66016D8bF4a981dfA826999f73D2c2937dA06eD'
+// const SAFEBOX_ADDRESS = '0xd66016D8bF4a981dfA826999f73D2c2937dA06eD'
+const SAFEBOX_ADDRESS = '0x27370bc7be6584931bf365014cf3ae0b5630ac72'
 
 var provider
 
-async function main() {
+async function create() {
     const accounts = await hre.ethers.getSigners()
     provider = accounts[0].provider
-    console.log('deployer: ', accounts[0].address)
-
-    const ZKPass = await ethers.getContractFactory('ZKPass')
-    const zkPass = await ZKPass.attach(ZKPASS_ADDRESS)
+    const delayer = accounts[0].address
+    console.log('caller: ', delayer)
 
     const SafeboxFactory = await ethers.getContractFactory('SafeboxV2Factory')
     const safeboxFactory = await SafeboxFactory.attach(SAFEBOXFACTORY_ADDRESS)
 
-    // let predictedAddress = await safeboxFactory.newSafeboxAddr(accounts[0].address)
-    // console.log('safebox predictedAddress', predictedAddress)
-    const Safebox = await ethers.getContractFactory('SafeboxV2')
-    let safebox = await Safebox.attach(SAFEBOX_ADDRESS)
+    let predictedAddress = await safeboxFactory.newSafeboxAddr(delayer)
+    console.log('safebox predictedAddress', predictedAddress)
 
-    // const MockERC20 = await ethers.getContractFactory('MockERC20')
-    // const usdt = await MockERC20.attach(USDC_ADDRESS)
-
-    // await usdt.transfer(predictedAddress, m(1, 18))
-
-    //createSafebox
-    // let pwd = '***'
-    // let nonce = '1'
-    // let datahash = '0'
-    // let p = await getProof(pwd, safebox.address, nonce, datahash)
-    // let tx = await safeboxFactory.createSafebox(p.proof, p.pwdhash, p.expiration, p.allhash)
-    // console.log('createSafebox submitted')
-    // let receipt = await tx.wait()
-    // console.log('createSafebox confirmed, address:', b(receipt.logs[2].topics[2]).toHexString())
-
-    //setup privatekey
-    // pwd = '***'
-    // nonce = s(await zkPass.nonceOf(safebox.address))
-    // datahash = s(b(accounts[0].address))
-    // p = await getProof(pwd, safebox.address, nonce, datahash)
-    // await safebox.resetPk(accounts[0].address, p.proof, p.expiration, p.allhash)
-    // console.log('resetPk done')
-
-    // await delay(10)
-    console.log('safebox pkAddr:', await safebox.pkAddr())
+    // 创建保险箱
+    let pwd = '123456'
+    let nonce = '1'
+    let datahash = '0'
+    let p = await getProof(pwd, predictedAddress, nonce, datahash)
+    let tx = await safeboxFactory.createSafebox(p.proof, p.pwdhash, p.expiration, p.allhash)
+    console.log('createSafebox submitted')
+    let receipt = await tx.wait()
+    console.log('createSafebox confirmed, safebox address is:', b(receipt.logs[2].topics[2]).toHexString())
 
     console.log('done')
 }
 
+
+async function login() {
+    const accounts = await hre.ethers.getSigners()
+    provider = accounts[0].provider
+    const user = accounts[0].address
+    console.log('caller: ', user)
+
+    const ZKPass = await ethers.getContractFactory('ZKPass')
+    const zkPass = await ZKPass.attach(ZKPASS_ADDRESS)
+
+    //用户名密码
+    let id = SAFEBOX_ADDRESS
+    let pwd = '123456'
+
+    //登录
+    let nonce = s(await zkPass.nonceOf(SAFEBOX_ADDRESS))
+    let datahash = '0'
+    let p = await getProof(pwd, SAFEBOX_ADDRESS, nonce, datahash)
+
+    if (p.pwdhash == s(await zkPass.pwdhashOf(SAFEBOX_ADDRESS))) {
+        console.log('密码正确')
+
+        const Safebox = await ethers.getContractFactory('SafeboxV2')
+        const safebox = await Safebox.attach(SAFEBOX_ADDRESS)
+
+        if (await safebox.pkAddr() == '0x0000000000000000000000000000000000000000') {
+            //新用户，未绑定私钥，引导他绑定
+            datahash = s(b(user))
+            p = await getProof(pwd, safebox.address, nonce, datahash)
+            await safebox.resetPk(user, p.proof, p.expiration, p.allhash)
+            console.log('resetPk done, enter app..')
+        } else {
+            //老用户，已绑定私钥，直接进入app
+            console.log('safebox had pk, enter app..')
+        }
+        
+    } else {
+        console.log('密码错误')
+    }
+
+    console.log('done')
+}
+
+
+
+
 //util
 async function getProof(pwd, address, nonce, datahash) {
-
     let expiration = parseInt(Date.now() / 1000 + 600)
     let chainId = (await provider.getNetwork()).chainId
     let fullhash = utils.solidityKeccak256(['uint256', 'uint256', 'uint256', 'uint256'], [expiration, chainId, nonce, datahash])
@@ -142,7 +168,7 @@ function s(bn) {
     return bn.toString()
 }
 
-main()
+login()
     .then(() => process.exit(0))
     .catch(error => {
         console.error(error);
